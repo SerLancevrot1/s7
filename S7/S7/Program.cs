102365298e9e1b83bc248e80b441b474c0b6165c
SerLancevrot1 <50653942+SerLancevrot1@users.noreply.github.com>
2019-12-11 16:27:21 +0300

    Add files via upload

---
+++ S7/S7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Sharp7;
+
+namespace S7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new S7Client();
+            int result = client.ConnectTo("127.0.0.1", 0, 1);
+            if (result == 0)
+            {
+                Console.WriteLine("Connected to 127.0.0.1");
+            }
+            else
+            {
+                Console.WriteLine(client.ErrorText(result));
+            }
+            Console.Read();
+
+
+            // Disconnect the client
+          //  client.Disconnect();
+        }
+    }
+}
